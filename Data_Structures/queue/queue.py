f08afa03b76c3f679e4810a4181d0feddcda0ffa
Jonathan Xu <jonathanj.xu@yahoo.com>
2017-01-28 11:27:18 -0600

    Data_Structures/queue

---
+++ Data_Structures/queue/queue.py
@@ -0,0 +1,43 @@
+class queue:
+  def __init__(self):
+    self.inStack = []
+    self.outStack = []
+
+  def enqueue(self, x):
+    self.inStack.append(x)
+
+  def dequeue(self):
+    if len(self.inStack) == 0 and len(self.outStack) == 0:
+      raise Exception('Queue is empty')
+    if len(self.outStack) == 0:
+      while len(self.inStack) > 0:
+        self.outStack.append(self.inStack.pop())
+    return self.outStack.pop()
+
+  def front(self):
+    if len(self.inStack) == 0 and len(self.outStack) == 0:
+      raise Exception('Queue is empty')
+    if len(self.outStack) == 0:
+      while len(self.inStack) > 0:
+        self.outStack.append(self.inStack.pop())
+    return self.outStack[len(self.outStack)-1]
+
+def main():
+  q = queue()
+  q.enqueue(5)
+  q.enqueue(4)
+  q.enqueue(3)
+  q.enqueue(2)
+  q.enqueue(1)
+  
+  print(q.front())
+  print(q.dequeue())
+  print(q.dequeue())
+  print(q.dequeue())
+  q.enqueue(23)
+  print(q.dequeue())
+  print(q.dequeue())
+  print(q.dequeue())
+
+if __name__ == '__main__':
+  main()
